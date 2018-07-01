ff59d332c9bf9820a96e235879253e6ccf16a5bf
Nyarlathotepss <40202859+Nyarlathotepss@users.noreply.github.com>
2018-07-01 14:57:37 +0200

    creation de la fonction

---
+++ partie_4.py
@@ -0,0 +1,5 @@
+def partie_4(lettre):
+    import labyrinthe
+    lettre_a_saisir = input("Veuillez saisir une lettre :")
+    if lettre_a_saisir in labyrinthe.values():
+        print(labyrinthe.keys)
