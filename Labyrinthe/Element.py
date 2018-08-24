43318dc2bd5543bdd010cc2fa05e0caaaae37df7
Nyarlathotepss <rcp.nmillet@gmail.com>
2018-08-24 10:49:45 +0200

    ajout classe et structure du programme

--- Labyrinthe/Element.py
+++ Labyrinthe/Element.py
@@ -1,8 +1,30 @@
 class Element:
 
-    def __init__(self):
-        self.
+    def __init__(self,):
+        self.positionx = int
+        self.positiony = int
 
-    def Check_Position(self):
 
+class Player(Element):
 
+    def checkposition(self):
+        for cle,valeur in dict_labyrinthe.items():
+            if valeur == "m":
+                return cle
+
+class Block(pygame.sprite.Sprite):
+
+        # Constructor. Pass in the color of the block,
+        # and its x and y position
+        def __init__(self, color, width, height):
+            # Call the parent class (Sprite) constructor
+            pygame.sprite.Sprite.__init__(self)
+
+            # Create an image of the block, and fill it with a color.
+            # This could also be an image loaded from the disk.
+            self.image = pygame.Surface([width, height])
+            self.image.fill(color)
+
+            # Fetch the rectangle object that has the dimensions of the image
+            # Update the position of this object by setting the values of rect.x and rect.y
+            self.rect = self.image.get_rect()
--- Labyrinthe/execute.py
+++ Labyrinthe/execute.py
@@ -1,95 +1,89 @@
-import fonctions
 import pygame
 import random
 from pygame.locals import *
 
-pygame.init()
-
-fenetre = pygame.display.set_mode((640, 480),RESIZABLE)
-
-fond = pygame.image.load("fond-gris.jpg").convert()
-
-fenetre.blit(fond,(0,0))
-pygame.display.flip()
-
-continuer = 1
-while continuer:
-    for event in pygame.event.get():  # On parcours la liste de tous les événements reçus
-        if event.type == QUIT:  # Si un de ces événements est de type QUIT
-            continuer = 0  # On arrête la boucle
-
-'''
 #On génére le labyrinthe en dictionnaire
-dict_labyrinthe = fonctions.create_dict_labyrinthe()
-print(dict_labyrinthe)
+my_file = open("labyrinthe.txt", "r")
+text = my_file.read()
+dict_labyrinthe = {}
+nb_lettre = 0  # Bloc pour compter le nombre de lettre dans une ligne
+key1 = 0
+key2 = 0
+for lettre in text:
+    nb_lettre += 1
+    if lettre == "\n":
+        nb_lettre -= 1  # Ici on enleve le saut de ligne \n du comptage de lettre
+        break
+for lettre in text:
+    if lettre != "\n":
+        dict_labyrinthe[key1,key2] = lettre
+        key1 += 1
+        if key1 == nb_lettre:
+            key1 = 0
+            key2 += 1
 
 #On genere les 3 objets
- Nous allons generer un 2 eme dictionnaire qui ne contiendra que les tuples ayant pour valeur "-"
-puis nous recupererons en random 3 postitions où nous placerons nos objets.
-
+'''Nous allons generer un 2 eme dictionnaire qui ne contiendra que les tuples ayant pour valeur "-"
+puis nous recupererons en random 3 positions où nous plaçerons nos objets.'''
 list_empty_case = []
 for cle,valeur in dict_labyrinthe.items():
     if valeur == "-":
         list_empty_case.append(cle)
-print(list_empty_case)
-
 position_object1 = random.choice(list_empty_case)
 position_object2 = random.choice(list_empty_case)
 position_object3 = random.choice(list_empty_case)
-
-print(position_object1,position_object2,position_object3)
-
 dict_labyrinthe[position_object1] = "a"
 dict_labyrinthe[position_object2] = "b"
 dict_labyrinthe[position_object3] = "c"
 
-print(dict_labyrinthe)
+# Ici commence l'interface graphique via pygame
+pygame.init()
+fenetre = pygame.display.set_mode((640, 480),RESIZABLE)
+fond = pygame.image.load("fond-gris.jpg").convert()
+fenetre.blit(fond,(0,0))
 
+character = pygame.image.load("MacGyver.png").convert_alpha()
+position_char = character.get_rect()
 
+pygame.display.flip()
 
-#perso = pygame.image.load("MacGyver.png").convert()
+continuer = 1
+while continuer:
 
-continu = 1
-while continu:
-# Ici on génére la boucle du labyrinthe
-    play = True
-    while play:
+    for event in pygame.event.get():  # On parcours la liste de tous les événements reçus
 
-        ligne = str()
-        list_lettre = []
-        i = 0
+        if event.type == QUIT:  # Si un de ces événements est de type QUIT
+            continuer = 0  # On arrête la boucle
 
-        # Puis on l'affiche a l'utilisateur via l'aide d'une liste
-        for lettre in dict_labyrinthe.values():
-            list_lettre.append(lettre)
-            i += 1
-            if i > 15:
-                ligne = "".join(list_lettre)
-                print(ligne)
-                ligne = None
-                i = 0
-                list_lettre = []
-
-        # check de la position de mac gyver
-        for cle,valeur in dict_labyrinthe.items():
-            if valeur == "m":
-                position = cle
-        print(position)
-
-        # Demande de direction a l'utilisateur qui va modifier la position de mc gyver
-        move_input = input("saisir une direction:")
-        if move_input == "q":
+        print("Please choice a direction")
+        #pygame.event.wait()
+        if event.type == K_LEFT:
             dict_labyrinthe[position] = "-"
-            dict_labyrinthe[(position[0]) - 1,position[1]] = "m"
-        if move_input == "d":
+            dict_labyrinthe[(position[0]) - 1, position[1]] = "m"
+            position_char = position_char.move()
+        if event.type == K_RIGHT:
             dict_labyrinthe[position] = "-"
-            dict_labyrinthe[(position[0]) + 1,position[1]] = "m"
-        if move_input == "s":
+            dict_labyrinthe[(position[0]) + 1, position[1]] = "m"
+            position_char = position_char.move()
+        if event.type == K_DOWN:
             dict_labyrinthe[position] = "-"
             dict_labyrinthe[(position[0]), (position[1] + 1)] = "m"
-        if move_input =="z":
+            position_char = position_char.move()
+        if event.type == K_UP:
             dict_labyrinthe[position] = "-"
             dict_labyrinthe[(position[0]), (position[1] - 1)] = "m"
+            position_char = position_char.move()
 
-        print(dict_labyrinthe)
-'''
+    # Creation du labyrinthe à afficher à l'aide d'une liste
+    ligne = str()
+    list_lettre = []
+    i = 0
+    for lettre in dict_labyrinthe.values():
+        list_lettre.append(lettre)
+        i += 1
+        if i > 15:
+            ligne = "".join(list_lettre)
+            print(ligne)
+            ligne = None
+            i = 0
+            list_lettre = []
