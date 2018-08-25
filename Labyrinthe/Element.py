764a9a3145173f90cb752e7b7ac28d6da2397b7b
Nyarlathotepss <40202859+Nyarlathotepss@users.noreply.github.com>
2018-08-25 11:48:01 +0200

    Update Element.py

--- Labyrinthe/Element.py
+++ Labyrinthe/Element.py
@@ -1,6 +1,6 @@
 class Element:
 
-    def __init__(self,):
+    def __init__(self):
         self.positionx = int
         self.positiony = int
 
@@ -28,3 +28,44 @@ class Block(pygame.sprite.Sprite):
             # Fetch the rectangle object that has the dimensions of the image
             # Update the position of this object by setting the values of rect.x and rect.y
             self.rect = self.image.get_rect()
+            
+class Dictionnary:
+    
+    def __init__(self):
+        '''By default my dictionnary is empty'''
+        self.name = ""
+    
+    def generatedictionnary(self):
+        ''' Generate a dictionnary with file.txt'''
+        my_file = open("labyrinthe.txt", "r")
+        text = my_file.read()
+        dict_labyrinthe = {}
+        nb_lettre = 0  # Bloc pour compter le nombre de lettre dans une ligne
+            key1 = 0
+            key2 = 0
+        for lettre in text:
+            nb_lettre += 1
+            if lettre == "\n":
+            nb_lettre -= 1  # Ici on enleve le saut de ligne \n du comptage de lettre
+            break
+        for lettre in text:
+            if lettre != "\n":
+                dict_labyrinthe[key1,key2] = lettre
+                key1 += 1
+                if key1 == nb_lettre:
+                    key1 = 0
+                    key2 += 1
+                    
+        def generateobject(self):
+            '''generate 3 objects in random position in dictionnary'''
+            list_empty_case = []
+            for cle,valeur in dict_labyrinthe.items():
+                if valeur == "-":
+                list_empty_case.append(cle)
+            position_object1 = random.choice(list_empty_case)
+            position_object2 = random.choice(list_empty_case)
+            position_object3 = random.choice(list_empty_case)
+            dict_labyrinthe[position_object1] = "a"
+            dict_labyrinthe[position_object2] = "b"
+            dict_labyrinthe[position_object3] = "c"
+            
