6fd7d5910fa8568c582de07e0d2ecb90fa0840a5
Nyarlathotepss <rcp.nmillet@gmail.com>
2018-09-02 17:44:51 +0200

    orienté objet dvlp affichage fentre pygame a terminer

--- Labyrinthe/fonctions.py
+++ Labyrinthe/fonctions.py
@@ -1,5 +1,3 @@
-# On genere un dict a partir du fichier txt
-
 def create_dict_labyrinthe(key1=0, key2=0):
     my_file = open("labyrinthe.txt", "r")
     text = my_file.read()
@@ -19,44 +17,16 @@ def create_dict_labyrinthe(key1=0, key2=0):
                 key2 += 1
     return labyrinthe
 
-def create_labyrinthe_to_print():
+def create_labyrinthe_to_print() :
+    my_file = open("labyrinthe.txt", "r")
+    text = my_file.read()
+    labyrinthe_to_print = []
+    for line in text:
+        labyrinthe_to_print = line.strip("\n")
+    return labyrinthe_to_print
 
-    ligne = str()
-    list_lettre = []
-    i = 0
-    for lettre in dict_labyrinthe.values():
-        list_lettre.append(lettre)
-        i += 1
-        if i > 15:
-            ligne = "".join(list_lettre)
-            print(ligne + "\n")
-            ligne = None
-            i = 0
-
-def check_mcgyver() :
-
-    from execute import dict_labyrinthe
-    if lettre == "m" in dict_labyrinthe.values():
-        position = dict_labyrinthe.keys()
-    return position
-
-def move_mcgyver():
-
+def move_mcgyver() :
     move_input = input("saisir une direction:")
-    if move_input == "q":
-        create_labyrinthe_to_print[(position)] = "-"
-        create_labyrinthe_to_print[(position)([0] - 1)] = "m"
-    if move_input == "d":
-        create_labyrinthe_to_print[x , y] = "-"
-        create_labyrinthe_to_print[(x + 1), y] = "m"
-    if move_input == "s":
-        create_labyrinthe_to_print[x, y] = "-"
-        create_labyrinthe_to_print[x , (y + 1)] = "m"
-    if move_input =="z":
-        create_labyrinthe_to_print[x , y] = "-"
-        create_labyrinthe_to_print[x , (y - 1)] = "m"
-
-
 
 
 
