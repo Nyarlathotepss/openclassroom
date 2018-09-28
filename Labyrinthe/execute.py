b68ca722f4d2d2133f71230c8b580fd00caf1a64
Nyarlathotepss <rcp.nmillet@gmail.com>
2018-09-28 10:51:50 +0200

    pep8 OK

--- Labyrinthe/execute.py
+++ Labyrinthe/execute.py
@@ -2,7 +2,8 @@ import pygame
 from pygame.locals import KEYDOWN, QUIT, K_LEFT, K_RIGHT, K_UP, K_DOWN
 from Element import Player, Dictionnary, Element
 
-dict = Dictionnary()
+'''create dictionnary then display window'''
+d = Dictionnary()
 
 pygame.init()
 
@@ -10,18 +11,18 @@ window = pygame.display.set_mode((300, 300))
 fond = pygame.image.load("fond-gris.jpg").convert()
 window.blit(fond, (0, 0))
 
-dict.displayelement(window)
+d.displayelement(window)
 
 pygame.display.flip()
 
-macgyver = Player(dict)
-guardian = Player(dict)
-
+'''game loop - waiting event'''
+macgyver = Player(d)
+guardian = Player(d)
 continuer = 1
 while continuer:
 
-    position = macgyver.checkposition(dict)
-    positionguardian = guardian.checkguardianposition(dict)
+    position = macgyver.checkposition(d)
+    positionguardian = guardian.checkguardianposition(d)
 
     for event in pygame.event.get():
 
@@ -30,26 +31,26 @@ while continuer:
 
         if event.type == KEYDOWN:
             if event.key == K_LEFT:
-                if dict.dict_labyrinthe[position[0] - 1, position[1]] != "w":
-                    dict.dict_labyrinthe[position[0], position[1]] = "-"
-                    dict.dict_labyrinthe[(position[0]) - 1, position[1]] = "m"
+                if d.dict_labyrinthe[position[0] - 1, position[1]] != "w":
+                    d.dict_labyrinthe[position[0], position[1]] = "-"
+                    d.dict_labyrinthe[(position[0]) - 1, position[1]] = "m"
             if event.key == K_RIGHT:
-                if dict.dict_labyrinthe[position[0] + 1, position[1]] != "w":
-                    dict.dict_labyrinthe[position] = "-"
-                    dict.dict_labyrinthe[(position[0]) + 1, position[1]] = "m"
+                if d.dict_labyrinthe[position[0] + 1, position[1]] != "w":
+                    d.dict_labyrinthe[position] = "-"
+                    d.dict_labyrinthe[(position[0]) + 1, position[1]] = "m"
             if event.key == K_DOWN:
-                if dict.dict_labyrinthe[position[0], position[1] + 1] != "w":
-                    dict.dict_labyrinthe[position] = "-"
-                    dict.dict_labyrinthe[(position[0]), (position[1] + 1)] = "m"
+                if d.dict_labyrinthe[position[0], position[1] + 1] != "w":
+                    d.dict_labyrinthe[position] = "-"
+                    d.dict_labyrinthe[(position[0]), (position[1] + 1)] = "m"
             if event.key == K_UP:
-                if dict.dict_labyrinthe[position[0], position[1] - 1] != "w":
-                    dict.dict_labyrinthe[position] = "-"
-                    dict.dict_labyrinthe[(position[0]), (position[1] - 1)] = "m"
-            macgyver.checkobject(dict)
+                if d.dict_labyrinthe[position[0], position[1] - 1] != "w":
+                    d.dict_labyrinthe[position] = "-"
+                    d.dict_labyrinthe[(position[0]), (position[1] - 1)] = "m"
+            macgyver.checkobject(d)
 
-            position = macgyver.checkposition(dict)
+            position = macgyver.checkposition(d)
             window.blit(fond, (0, 0))
-            dict.displayelement(window)
+            d.displayelement(window)
             pygame.display.flip()
             print(position, positionguardian, macgyver.checkallobjects(), macgyver.listobjects)
         if position == positionguardian:
